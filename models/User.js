5dba3ae68040056ddd8e69e512c76f133a28304a
Zack <hzkzqxx@163.com>
2021-01-05 20:26:18 -0800

    112

--- models/User.js
+++ models/User.js
@@ -3,6 +3,8 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
     googleId : String,
+    //attribute for number of credits
+    credits: { type:Number, default: 0},
 });
 
 mongoose.model("users", userSchema);
--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -3,6 +3,10 @@ const stripe = require("stripe")(keys.stripeSecretKey);
 
 module.exports = app =>{
     app.post('/api/stripe', async (req,res) => {
+        if(!req.user){
+            return res.status(401).send({ error: "You must log in!"});
+        }
+
         //back-end operation for charge using stripe
         const charge = stripe.charges.create({
             amount: 500,
@@ -10,7 +14,13 @@ module.exports = app =>{
             description: '$5 for 5 credits',
             source: req.body.id,
         });
-
-        console.log(charge);
+    
+        //Get the user credit attribute and add 5
+        req.user.credits += 5;
+        const user = await req.user.save();
+        //Send the user information back to the browser
+        res.send(user);
     });
+
+
 };

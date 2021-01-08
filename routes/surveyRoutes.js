32fa66a7a876cb8c558003126451e8cffedcf33e
Zack <hzkzqxx@163.com>
2021-01-07 20:23:45 -0800

    136

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -8,10 +8,15 @@ module.exports = app => {
     app.post('/api/surveys', requireLogin, requireCredit, (req,res) => {
         const { title, subject, body, recipients } = req.body;
         //Create a instance in Survey collection
-        const survey = new Survey{
+        const survey = new Survey({
             title,
             subject,
             body,
-        }
+            //take the email address, split using "," and the using map to change each splited string to object{} 
+            recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+            _user: req.user.id,
+            dateSent: Date.now(),
+
+        })
     });
 };

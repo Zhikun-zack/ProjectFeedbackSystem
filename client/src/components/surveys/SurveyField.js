25e8d09f49c75a62235834e18b45cfb64eb146e2
Zack <hzkzqxx@163.com>
2021-01-13 06:56:24 -0800

    159

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,10 @@
+//SurveyFeild contains logic to render each label and input
+import React from "react";
+
+export default () => {
+    return(
+        <div>
+            SurveyField
+        </div>
+    )
+}
--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,12 +1,27 @@
 //SurveyForm shows a form for a user to add input
 import React, { Component } from "react";
-import { reduxForm } from "redux-form";
+//Field is used to render all the HTML form type e.g. checkbox radiobox
+import { reduxForm, Field } from "redux-form";
+import SurveyFeild from "./SurveyField";
 
 class SurveyForm extends Component{
+    //render the fields using the field defined myself not using Field library directly
+    renderFields(){
+        return (
+            <div>
+                <Field type = "text" name = "title" component = {SurveyFeild}></Field>
+            </div>
+        )
+    }
     render( ){
         return (
             <div>
-                SurveyForm
+                {/** onSubmit is working when button which type is submit be clicked and the value is the words input*/}
+                <form onSubmit = {this.props.handleSubmit(value => console.log(value))}>
+                    {this.renderFields()}
+                    <button type = "submit" >Submit</button>
+                </form>
+                
             </div>
         )
     }

59fd7e75a1b9293c4922ffe3444d2b41e6bb6703
Zack <hzkzqxx@163.com>
2021-01-18 05:42:06 -0800

    another

--- index.js
+++ index.js
@@ -30,20 +30,18 @@ require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 require('./routes/surveyRoutes')(app);
 
-if (process.env.NODE_ENV === "production"){
-    /**Express will serve up production assets
-     * like our main.js file or main.css file
-     */
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets
+    // like our main.js file, or main.css file!
     app.use(express.static('client/build'));
-    /**
-     * Express will serve up the index.html file
-     * if it doesnt recognize the route
-     */
+  
+    // Express will serve up the index.html file
+    // if it doesn't recognize the route
     const path = require('path');
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
-}
+  }
 
 //Dynamic Port binding, related with port imported in cmd command 
 const PORT = process.env.PORT || 5000;

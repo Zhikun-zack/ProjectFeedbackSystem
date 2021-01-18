dfd26747dcec542697312b87e154c4a71abea021
Zack <hzkzqxx@163.com>
2021-01-18 01:57:23 -0800

    index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ if (process.env.NODE_ENV === "production"){
      * if it doesnt recognize the route
      */
     const path = require('path');
-    app.get('*',(req,res)=>{
+    app.use('*',(req,res)=>{
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
 }

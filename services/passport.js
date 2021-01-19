9d84a02a61457f00c042fc20eb17c009e7c4d315
Zack <hzkzqxx@163.com>
2021-01-18 17:41:10 -0800

    ! Add judgement in passport.js and in different envi will execute different codes!

--- services/passport.js
+++ services/passport.js
@@ -15,23 +15,54 @@ passport.deserializeUser((id, done) => {
       done(null, user);
     });
   });
-passport.use(
-    new GoogleStrategy(
-      {
-        clientID: keys.googleClientID,
+
+//In production envi we cannot use proxy otherwise we will meet internal server error
+if(process.env.NODE_ENV === "production"){
+    passport.use(
+        new GoogleStrategy(
+          {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback',
+            proxy: true
+          },
+          async (accessToken, refreshToken, profile, done) => {
+            const existingUser = await User.findOne({ googleId: profile.id });
+      
+            if (existingUser) {
+              return done(null, existingUser);
+            }
+      
+            const user = await new User({ googleId: profile.id }).save();
+            done(null, user);
+          }
+        )
+      );
+}
+//In dev envioronment we have to use the http proxy to help data send through the firewall
+else {
+    //Create a strategy which will be used by passport
+    const gStartegy = new GoogleStrategy({
+        clientID : keys.googleClientID,
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback',
         proxy: true
-      },
-      async (accessToken, refreshToken, profile, done) => {
-        const existingUser = await User.findOne({ googleId: profile.id });
-  
-        if (existingUser) {
-          return done(null, existingUser);
-        }
-  
-        const user = await new User({ googleId: profile.id }).save();
-        done(null, user);
-      }
-    )
-  );
+        },
+        //error function
+        async (accessToken, refreshToken, profile, done) => {
+            const existingUser = await User.findOne({ googleId: profile.id });
+    
+            if (existingUser) {
+            return done(null, existingUser);
+            }
+    
+            const user = await new User({ googleId: profile.id }).save();
+            done(null, user);
+        });
+    //set agent if behind firewall
+    const agent = new HttpsProxyAgent(process.env.HTTP_PROXY || "http://localhost:1080");
+    gStartegy._oauth2.setAgent(agent);
+
+    //passport end strategyasd
+    passport.use(gStartegy);
+}

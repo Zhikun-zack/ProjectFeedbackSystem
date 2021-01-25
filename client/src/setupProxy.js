ec56d8a14ad16c882881d827e5ebd133899ade16
Zack <hzkzqxx@163.com>
2021-01-25 02:54:39 -0800

    add commit 71

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,5 +1,6 @@
 const {createProxyMiddleware} = require("http-proxy-middleware");
 
+//Set up a proxy when we need to access the backend data using front end 
 module.exports = app => {
     app.use(
         ['/api','/auth/google'],
